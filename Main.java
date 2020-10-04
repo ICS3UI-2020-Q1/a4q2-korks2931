3b91f4f29ed47467c1544ede6f78efe847e18e4e
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 14:42:14 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,38 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will keep telling the user to enter a positive integer until they enter a negative integer.
+ * @author Sevde
  */
-public class Main {
+public class Main {  
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input 
+    Scanner input = new Scanner(System.in);
     
-  }
+    // create a variable for integer
+    int integer;
+      
+      // create a do while loop 
+    do{ 
+      // ask the user for the integer
+      System.out.println("Please enter a postive integer.Enter a negative integer to quit.");
+      integer = input.nextInt();
+      // create a while loop
+      }while(integer >= 0);
+      // tell the user the game is done
+      System.out.println("All done!");
+      
+      
+      
+      }
 }
+
+    
+  
+    
+
+
